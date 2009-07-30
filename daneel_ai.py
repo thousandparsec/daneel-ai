30c799a76c58f1342dd9e99c172a3728e1f5bffc
ric <ric@ric-ubuntu-nine.(none)>
2009-07-31 07:03:46 +1000

    daneel_ai.py: modified to use pyke knowledge engine. removed the need to read text files to load the correct game module

--- daneel_ai.py
+++ daneel_ai.py
@@ -20,16 +20,19 @@ from tp.netlib import failed, constants, objects
 from tp.client.cache import Cache
 
 import daneel
-from daneel.rulesystem import RuleSystem, BoundConstraint
-import picklegamestate
 import cPickle
-from constraint import *
-from problem import *
+from pyke import knowledge_engine
 
 version = (0, 0, 3)
-mods = []
-debug = 1
 
+def importGameFiles(rulesfile):
+
+	try:
+		game_file = getattr(__import__("daneel." + rulesfile), rulesfile)
+		return game_file.Game() 
+	except:
+		raise Exception, "Game rules missing for %s" % rulesfile
+	 
 def callback(mode, state, message="", todownload=None, total=None, amount=None):
     logging.getLogger("daneel").debug("Downloading %s %s Message:%s", mode, state, message)
 
@@ -74,30 +77,54 @@ def setLoggingLevel(verbosity, stream=sys.stdout):
     fmt = "%(asctime)s [%(levelname)s] %(name)s:%(message)s"
     logging.basicConfig(level=level,stream=sys.stdout,format=fmt)
     return
+   
+def getDataDir():
+    
+    if hasattr(sys, "frozen"):
+        installpath = os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding( )))
+    else:
+        installpath = os.path.realpath(os.path.dirname(__file__))
+        
+    if hasattr(sys, "frozen"):
+        return os.path.join(installpath, "share", "daneel-ai")
+    if "site-packages" in daneel.__file__:
+        datadir = os.path.join(os.path.dirname(daneel.__file__), "..", "..", "..", "..", "share", "daneel-ai")
+    else:
+        datadir = os.path.join(os.path.dirname(daneel.__file__), "..")
+    return datadir
+
+def stripline(line):
+    if line[0] == "#": return ""
+    return line.strip()
 
 
-def startTurn(cache,store, delta):
-    for m in store.mods:
-        try:
-             m.startTurn(cache,store, delta)
-        except AttributeError:
-            pass
-           
-    return list
+def saveGame(cache,rulesfile,verbosity):
+    root_dir = getDataDir()
+    save_dir = os.path.join( root_dir, "states" )
+    writeable = checkSaveFolderWriteable(root_dir, save_dir)
+    # NB assumes there is enough space to write
+    if not writeable:
+        logging.getLogger("daneel").error("Cannot save information")
+    else:       
+        cache.file = os.path.join(save_dir, rulesfile + "_-_" + str(verbosity) +
+								   "_-_" + time.time().__str__() + ".gamestate")
+        cache.save()
 
-def endTurn(cache,rulesystem,connection):
-    for m in rulesystem.mods:
-        try:
-            m.endTurn(cache,rulesystem,connection)
-        except AttributeError:
-            pass
 
-def init(cache,rulesystem,connection):
-    for m in rulesystem.mods:
-        try:
-            m.init(cache,rulesystem,connection)
-        except AttributeError:
-            pass
+def checkSaveFolderWriteable(root_dir, save_dir):    
+    dir_exists = os.access(save_dir, os.F_OK)
+    dir_writeable = os.access(save_dir, os.W_OK)
+    dir_root_writeable = os.access(root_dir, os.W_OK)
+    if dir_exists and dir_writeable:
+        return True
+    if dir_exists and not dir_writeable:
+        return False
+    if dir_root_writeable:
+        os.mkdir(save_dir)
+        return True
+    else:
+        return False
+
 
 def gameLoop(rulesfile,turns=-1,uri='tp://daneel-ai:cannonfodder@localhost/tp',verbosity=1,save=0):
     setLoggingLevel(verbosity)
@@ -105,34 +132,32 @@ def gameLoop(rulesfile,turns=-1,uri='tp://daneel-ai:cannonfodder@localhost/tp',v
     gameLoopWrapped(rulesfile,turns,connection,cache,verbosity,save)
 
 def gameLoopWrapped(rulesfile,turns,connection,cache,verbosity,save):
+
+
+    daneel_engine = knowledge_engine.engine('rules')        
+    game = importGameFiles(rulesfile)
 			
-    problem = DaneelProblem(rulesfile,verbosity)
     logging.getLogger("daneel").info("Downloading all data")
     cache.update(connection,callback)
-#    state = picklegamestate.GameState(rulesfile,turns,None,cache,verbosity)
- #   state.pickle("./states/" + time.time().__str__() + ".gamestate")
+    game.init(cache,daneel_engine,connection)
 
-    init(cache,problem,connection)
-    
-    delta = True
     while turns != 0:
         turns = turns - 1
+
         logging.getLogger("daneel").info("Downloading updates")
         cache.update(connection,callback)
+        
         # store the cache
         if save == '1':
-            print 'saving'
+            logging.getLogger("daneel").info("Saving Cache")
             saveGame(cache,rulesfile,verbosity) 
         
         lastturn = cache.objects[0].turn
 
-        startTurn(cache,problem,delta)
-        # rulesystem.addConstraint("cacheentered")
-        endTurn(cache,problem,None)
-        problem.resetVariables()
-
-        if(debug):
-            time.sleep(10)
+        game.startTurn(cache,daneel_engine)
+        game.endTurn(cache,daneel_engine,connection)
+        #if(debug):
+         #   time.sleep(10)
             
         connection.turnfinished()
        
@@ -142,17 +167,18 @@ def gameLoopWrapped(rulesfile,turns,connection,cache,verbosity,save):
             waitfor = connection.time()
             time.sleep( max(1,waitfor / 10))
 
+
 def gameLoopBenchMark(rulesfile,turns,connection,cache,verbosity):
-    setLoggingLevel(verbosity)   
-    problem = DaneelProblem(rulesfile,verbosity)
+    setLoggingLevel(verbosity)
+    #setLoggingLevel(2)
+    daneel_engine = knowledge_engine.engine('rules')
+    daneel_engine.reset()        
     logging.getLogger("daneel").info("Running BenchMark")
-    init(cache,problem,connection)
+    game = importGameFiles(rulesfile)
     
-    delta = False
-    startTurn(cache,problem,delta)
-    
-    endTurn(cache,problem,None)
-    problem.resetVariables()    
+    game.init(cache,daneel_engine,connection)
+    game.startTurn(cache,daneel_engine)
+    game.endTurn(cache,daneel_engine,None)
     return
 
 
